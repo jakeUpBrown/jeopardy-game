559cd6804417a0c8f78c02f8544b4c1d0194c432
Glitch (hello-webpage) <none>
2019-01-08 23:26:51 +0000

    🚦📙 Checkpoint ./script.js:2734036/571 ./index.html:2734036/105

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var handlers = {
- getCurrentDate: function()
+ doButtonStuff: function()
   {
-    var date = new Date();
+    debugger;
+    console.log('in doButtonStuff');
     
+    var dateField = document.getElementById('pField');
+    var oldString = dateField.textContent;
     
+    var newString = oldString[oldString.length - 1] + oldString.substring(0, oldString.length - 2)
+    dateField.textContent = newString;
   }
-  
 };
