58a6e6b6f3dd6695d30f7d77f73adef1813d95fd
Glitch (hello-webpage) <none>
2019-01-08 23:37:50 +0000

    🐩😅 Checkpoint ./script.js:2734036/578 ./index.html:2734036/307

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var handlers = {
- doButtonStuff: function()
-  {
-    debugger;
-    console.log('in doButtonStuff');
-    
+ animateString: function()
+  {    
     var dateField = document.getElementById('pField');
-    var oldString = dateField.textContent;
-    
-    var newString = oldString[oldString.length - 1] + oldString.substring(0, oldString.length - 2)
+    var oldString = dateField.textContent.trim();
+    var newString = oldString[oldString.length - 1] + oldString.substring(0, oldString.length - 1)
     dateField.textContent = newString;
   }
 };
+
+setInterval(handlers.animateString, 500);
+
+function isLeapYear(year)
+{
+  return year % 4 == 0 && year % 2000 != 0;
+}
