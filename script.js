06c4f52a70b06e1f338a10ab0f2e592d4fe15401
Glitch (hello-webpage) <none>
2019-01-09 02:50:56 +0000

    🚔✌️ Checkpoint ./script.js:2734036/777 ./index.html:2734036/26

--- script.js
+++ script.js
@@ -7,17 +7,16 @@ var playerList=
   },
   getPlayerBoxString: function(index)
   {
-    var player = players[index];
+    var player = this.players[index];
     var playerName = player.playerName;
     var keyBound = player.keyBound;
     
     if(keyBound === undefined)
-      keyBound = '(no key bound)';
+      keyBound = 'no key bound';
       
-    return playerName + " " + keyBound;
+    return playerName + " (" + keyBound + ")";
   }
 };
-
 
 
 var handlers = {
@@ -27,38 +26,39 @@ var handlers = {
     var newPlayer = {playerName: playerNameInput.value};
     playerNameInput.value = '';
     window.playerList.addNewPlayer(newPlayer);
+    view.displayPlayers();
+  },
+  playerBoxSelected: function(event)
+  {
+    console.log(event);
   }
 };
+
 
 var view = 
 {
   displayPlayers: function()
   {
-    // for each player in window.playerList, create an element and display it
-  },
-  createPlayerElement: function(player)
-  {
-    // for this player, create an element for the DOM
+    // for each player, create an element for the DOM
    
     var playerBoxContainer = document.getElementById('playerBoxContainer');
     
+    debugger;
+    // clear player box container
     playerBoxContainer.innerHTML = '';
     
-    this.playerList.players.forEach(function(player){
+    this.playerList.players.forEach(function(player, index){
           
       var flexChildElement = document.createElement('div');
       flexChildElement.className = 'flex-child';
 
       var playerNameBox = document.createElement('label');
-      playerNameBox.textContent = playerList
+      playerNameBox.className = 'player-name-box';
+      playerNameBox.textContent = playerList.getPlayerBoxString(index);
+      
+      flexChildElement.appendChild(playerNameBox);
       
       playerBoxContainer.appendChild(flexChildElement);
     });
-    
   }
 };
-
-
-      <div class="flex-child">
-        <label class="player-name-box">player name 2</label>
-      </div>  
