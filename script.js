be87266d64f4c31a8815c2b30fb54caae425b583
Glitch (hello-webpage) <none>
2019-01-09 00:51:51 +0000

    👘👙 Checkpoint ./script.js:2734036/1948 ./index.html:2734036/807

--- script.js
+++ script.js
@@ -1,40 +1,19 @@
+
+
+
 var handlers = {
-  buttonClicked: function(event)
+  addNewUserVariable: function()
   {
-    if(event.target.id === 'multiplyButton')
-      view.setResultsArea(view.getFirstNumber() * view.getSecondNumber());
-    else if(event.target.id === 'divideButton')
-      view.setResultsArea(view.getFirstNumber() / view.getSecondNumber());
+    var variableName = document.getElementById('fileNameInput').value;
+    
+    fileExtension variableName.lastIndexOf('.');
+    
   }
-};
+}
 
-
-var view = {
-  setResultsArea: function(text)
+var stringOperations = {
+  getFileExtension: function(fileName)
   {
-    var resultsArea = document.getElementById('resultsArea');
-    resultsArea.textContent = text;
-    this.clearNumberInputs();
-  },
-  getFirstNumber: function()
-  {
-    return document.getElementById('firstNumberInput').valueAsNumber;    
-  },
-  getSecondNumber: function()
-  {
-    return document.getElementById('secondNumberInput').valueAsNumber;    
-  },
-  clearNumberInputs: function()
-  {
-    this.clearFirstNumber();
-    this.clearSecondNumber();
-  },
-  clearFirstNumber: function()
-  {
-    document.getElementById('firstNumberInput').value = '';
-  },
-  clearSecondNumber: function()
-  {
-    document.getElementById('secondNumberInput').value = '';
+    return variableName.substring(0,
   }
-};
+}
