2dba06d881bd94e563c7887fefe117a32a657aaf
Glitch (hello-webpage) <none>
2019-01-09 00:40:52 +0000

    💦🎲 Checkpoint ./script.js:2734036/4339

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 var handlers = {
- multiply: function()
-  {    
-    var firstNumberInput = document.getElementById('firstNumberInput');    
-    var secondNumberInput = document.getElementById('secondNumberInput');
-  },
-  divide: function()
-  {
-    
-  },
   buttonClicked: function(event)
   {
-    console.log(event);
+    if(event.target.id === 'multiplyButton')
+      view.setResultsArea(view.getFirstNumber() * view.getSecondNumber());
+    else if(event.target.id === 'divideButton')
+      view.setResultsArea(view.getFirstNumber() / view.getSecondNumber());
+  }
+};
+
+
+var view = {
+  setResultsArea: function(text)
+  {
+    var resultsArea = document.getElementById('resultsArea');
+    resultsArea.textContent = text;
+    this.clearNumberInputs();
+  },
+  getFirstNumber: function()
+  {
+    return document.getElementById('firstNumberInput').valueAsNumber;    
+  },
+  getSecondNumber: function()
+  {
+    return document.getElementById('secondNumberInput').valueAsNumber;    
+  },
+  clearNumberInputs: function()
+  {
+    this.clearFirstNumber();
+    this.clearSecondNumber();
+  },
+  clearFirstNumber: function()
+  {
+    document.getElementById('firstNumberInput').value = '';
+  },
+  clearSecondNumber: function()
+  {
+    document.getElementById('secondNumberInput').value = '';
   }
 };
