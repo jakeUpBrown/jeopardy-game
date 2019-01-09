a8c747e8c300f07c3b6cc5bcd56276ec4badf1d0
Glitch (hello-webpage) <none>
2019-01-09 02:40:55 +0000

    👫☄️ Checkpoint ./script.js:2734036/1999

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ var playerList=
   addNewPlayer: function(player)
   {
     this.players.push(player);
+  },
+  getPlayerBoxString: function(index)
+  {
+    var player = players[index];
+    var playerName = player.playerName;
+    var keyBound = player.keyBound;
+    
+    if(keyBound === undefined)
+      keyBound = '(no key bound)';
+      
+    return playerName + " " + keyBound;
   }
 };
 
@@ -28,6 +39,26 @@ var view =
   createPlayerElement: function(player)
   {
     // for this player, create an element for the DOM
+   
+    var playerBoxContainer = document.getElementById('playerBoxContainer');
+    
+    playerBoxContainer.innerHTML = '';
+    
+    this.playerList.players.forEach(function(player){
+          
+      var flexChildElement = document.createElement('div');
+      flexChildElement.className = 'flex-child';
+
+      var playerNameBox = document.createElement('label');
+      playerNameBox.textContent = playerList
+      
+      playerBoxContainer.appendChild(flexChildElement);
+    });
     
   }
 };
+
+
+      <div class="flex-child">
+        <label class="player-name-box">player name 2</label>
+      </div>  
